0d3a528f236a96c33cc09ee26f972656cb52ddbc
Kat Berge <kat.n.berge@gmail.com>
2020-07-29 11:34:30 -0500

    adds transition betwen running and idle animations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,17 +6,27 @@ public class Player : MonoBehaviour
 {
     public float speed;
     private Rigidbody2D rb;
+    private Animator anim;
     private Vector2 moveAmount;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
     {
         Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         moveAmount = moveInput.normalized * speed;
+        
+        // set isRunning bool to transition between idle and running animations
+        if (moveInput != Vector2.zero) { 
+            anim.SetBool("isRunning", true);
+        } 
+        else {
+            anim.SetBool("isRunning", false);
+        }
     }
 
     private void FixedUpdate()

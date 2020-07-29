e4f49a54a99a72d24b001994245c53063e65f2f3
Kat Berge <kat.n.berge@gmail.com>
2020-07-29 11:12:09 -0500

    adds player (movement) script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb;
+    private Vector2 moveAmount;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveAmount = moveInput.normalized * speed;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveAmount * Time.fixedDeltaTime);
+    }
+}

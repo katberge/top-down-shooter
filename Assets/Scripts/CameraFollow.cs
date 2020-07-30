8b2ebea188016e3918f66c18312239723ce58d9e
Kat Berge <kat.n.berge@gmail.com>
2020-07-30 17:27:10 -0500

    adds camera follow script and box coliders to prevent player from falling off teh sides

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform playerTransform;
+    public float speed;
+
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = playerTransform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerTransform != null) { // incase the player dies
+            float clampedX = Mathf.Clamp(playerTransform.position.x, minX, maxX);
+            float clampedY = Mathf.Clamp(playerTransform.position.y, minY, maxY);
+            transform.position = Vector2.Lerp(transform.position, new Vector2(clampedX, clampedY), speed);
+        }
+    }
+}

a0b86f6959b5f671febaf0abe90d7d2b9432dcca
Kat Berge <kat.n.berge@gmail.com>
2020-07-30 14:56:39 -0500

    adds movement to teh projectiles via a new script

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+
+    public float speed;
+    public float lifetime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, lifetime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.up * speed * Time.deltaTime);
+    }
+}

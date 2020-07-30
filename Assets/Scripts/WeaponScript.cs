5940fbf6428f7909d1e3b87db3e72e9323617500
Kat Berge <kat.n.berge@gmail.com>
2020-07-30 14:34:33 -0500

    adds frefabs of weapons and adds rotation scipt for the weapon

---
+++ Assets/Scripts/WeaponScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward); // forward is the x axis
+        transform.rotation = rotation;
+    }
+}

98903fa9a7e1bff38a530b26e1da69161fae86e0
Kat Berge <kat.n.berge@gmail.com>
2020-08-03 10:32:14 -0500

    adds general enemy script and applies it to the three enemies, also adds to projectile script to work with enemy script

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    public int health;
+
+    public void TakeDamage(int damageAmount)
+    {
+        health -= damageAmount;
+        if (health <= 0) {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -8,6 +8,7 @@ public class Projectile : MonoBehaviour
     public float speed;
     public float lifetime;
     public GameObject explosion;
+    public int damage;
 
     // Start is called before the first frame update
     void Start()
@@ -25,5 +26,14 @@ public class Projectile : MonoBehaviour
     {
         Instantiate(explosion, transform.position, Quaternion.identity);
         Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Enemy")
+        {
+            collision.GetComponent<EnemyScript>().TakeDamage(damage);
+            DestroyProjectile();
+        }
     }
 }

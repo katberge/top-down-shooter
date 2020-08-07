0b86b88aae979547015c6f0327149972db2a8c57
Kat Berge <kat.n.berge@gmail.com>
2020-08-07 12:31:35 -0500

    adds wave spawner

---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    [System.Serializable]
+    public class Wave 
+    {
+        public GameObject[] enemies;
+        public int count;
+        public float timeBetweenSpawns;
+    }
+
+    public Wave[] waves;
+    public Transform[] spawnPoints;
+    public float timeBetweenWaves;
+
+    private Wave currentWave;
+    private int currentWaveIndex;
+    private Transform player;
+    private bool finishedSpawning;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        StartCoroutine(StartNextWave(currentWaveIndex));
+    }
+
+    IEnumerator StartNextWave(int index)
+    {
+        yield return new WaitForSeconds(timeBetweenWaves);
+        StartCoroutine(SpawnWave(index));
+    }
+
+    IEnumerator SpawnWave(int index)
+    {
+        currentWave = waves[index];
+        for (int i = 0; i < currentWave.count; i++) {
+            if (player == null) {
+                yield break;
+            }
+            GameObject randomEnemy = currentWave.enemies[Random.Range(0, currentWave.enemies.Length)];
+            Transform randomSpot = spawnPoints[Random.Range(0, spawnPoints.Length)];
+            Instantiate(randomEnemy, randomSpot.position, randomSpot.rotation);
+
+            if (i == currentWave.count - 1) {
+                finishedSpawning = true;
+            } else {
+                finishedSpawning = false;
+            }
+
+            yield return new WaitForSeconds(currentWave.timeBetweenSpawns);
+        }
+    }
+
+    private void Update()
+    {
+        if (finishedSpawning == true && GameObject.FindGameObjectsWithTag("Enemy").Length == 0) {
+            finishedSpawning = false;
+
+            // if there is another wave
+            if (currentWaveIndex + 1 < waves.Length) {
+                currentWaveIndex++;
+                StartCoroutine(StartNextWave(currentWaveIndex));
+            } else {
+                Debug.Log("Game Finished");
+            }
+        }
+    }
+
+}

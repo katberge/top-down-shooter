239b8e73c6b5e921e4451d49b42663b7709c23a7
Kat Berge <kat.n.berge@gmail.com>
2020-08-07 11:01:00 -0500

    adds ranged enemy scipt and a way for it to shoot its bullets(called bugAttack)

---
+++ Assets/Scripts/BugAttackBullet.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BugAttackBullet : MonoBehaviour
+{
+    private Player playerScript;
+    private Vector2 targetPosition;
+    public float speed;
+    public int damage;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        playerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        targetPosition = playerScript.transform.position;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Vector2.Distance(transform.position, targetPosition) > 0.1f) {
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+        } else {
+            Destroy(gameObject);
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player") {
+            playerScript.TakeDamage(damage);
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/RangeEnemy.cs
+++ Assets/Scripts/RangeEnemy.cs
@@ -4,5 +4,48 @@ using UnityEngine;
 
 public class RangeEnemy : EnemyScript
 {
-    
+    public float stopDistance;
+    private float attackTime;
+    private Animator anim;
+    public Transform shotPoint;
+    public GameObject bugAttack;
+    private string direction = "Forward";
+
+    public override void Start()
+    {
+        base.Start();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update() 
+    {
+        if (player != null) {
+            if (Vector2.Distance(transform.position, player.position) > stopDistance) {
+                transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+            } 
+            if (Time.time >= attackTime) {
+                attackTime = Time.time + timeBetweenAttacks;
+                anim.SetTrigger("attack");
+            }
+
+            // rotate range enemy to face spider
+            float xDist = player.position.x - transform.position.x;
+            if (xDist < 0 && direction == "Forward") {
+                direction = "Backward";
+                transform.rotation = Quaternion.Euler(0, 180, 0);
+            } else if (xDist >= 0 && direction == "Backward") {
+                direction = "Forward";
+                transform.rotation = Quaternion.identity;
+            }
+        }
+    }
+
+    public void RangeAttack()
+    {
+        Vector2 direction = player.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward); // forward is the x axis
+        shotPoint.rotation = rotation;
+        Instantiate(bugAttack, shotPoint.position, shotPoint.rotation); 
+    }
 }

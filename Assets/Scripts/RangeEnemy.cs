14b9c17521fea97e7003751ac505c11f48b85764
Kat Berge <kat.n.berge@gmail.com>
2020-08-29 14:44:18 -0500

    fixes error mesage of trying to reference player when dead within ranged attack

--- Assets/Scripts/RangeEnemy.cs
+++ Assets/Scripts/RangeEnemy.cs
@@ -42,10 +42,12 @@ public class RangeEnemy : EnemyScript
 
     public void RangeAttack()
     {
-        Vector2 direction = player.position - transform.position;
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        Quaternion rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward); // forward is the x axis
-        shotPoint.rotation = rotation;
-        Instantiate(bugAttack, shotPoint.position, shotPoint.rotation); 
+        if (player != null) {
+            Vector2 direction = player.position - transform.position;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            Quaternion rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward); // forward is the x axis
+            shotPoint.rotation = rotation;
+            Instantiate(bugAttack, shotPoint.position, shotPoint.rotation);
+        } 
     }
 }

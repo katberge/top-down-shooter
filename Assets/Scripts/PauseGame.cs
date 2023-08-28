1e63ca5374d9e5c9434904ead329864210d136ec
Kat Berge <kat.n.berge@gmail.com>
2023-08-27 21:51:52 -0500

    changes sprite on pause button press

--- Assets/Scripts/PauseGame.cs
+++ Assets/Scripts/PauseGame.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PauseGame : MonoBehaviour
 {
+    [SerializeField] private Sprite pauseSprite;
+    [SerializeField] private Sprite playSprite;
+
     public void TogglePause()
     {
         if (Time.timeScale == 0)
@@ -19,10 +22,12 @@ public class PauseGame : MonoBehaviour
     private void Pause()
     {
         Time.timeScale = 0;
+        gameObject.GetComponent<UnityEngine.UI.Image>().sprite = playSprite;
     }
 
     private void Unpause()
     {
         Time.timeScale = 1;
+        gameObject.GetComponent<UnityEngine.UI.Image>().sprite = pauseSprite;
     }
 }

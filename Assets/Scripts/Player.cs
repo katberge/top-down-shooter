904131cc89008aa18aefc1713e12507b4946437f
Kat Berge <kat.n.berge@gmail.com>
2025-03-29 11:55:31 -0500

    Revert "update to unity 6 and remove warnings"
    This reverts commit d6f4afda261d566d10855f537a40011930ee8a66.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -21,21 +21,19 @@ public class Player : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
-        sceneTransitions = FindAnyObjectByType<SceneTransitions>();
+        sceneTransitions = FindObjectOfType<SceneTransitions>();
     }
 
     private void Update()
     {
         Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         moveAmount = moveInput.normalized * speed;
-
+        
         // set isRunning bool to transition between idle and running animations
-        if (moveInput != Vector2.zero)
-        {
+        if (moveInput != Vector2.zero) { 
             anim.SetBool("isRunning", true);
-        }
-        else
-        {
+        } 
+        else {
             anim.SetBool("isRunning", false);
         }
     }
@@ -49,8 +47,7 @@ public class Player : MonoBehaviour
     {
         health -= damageAmount;
         UpdateHealthUI(health);
-        if (health <= 0)
-        {
+        if (health <= 0) {
             Destroy(gameObject);
             sceneTransitions.LoadScene("Lose");
         }
@@ -64,14 +61,10 @@ public class Player : MonoBehaviour
 
     void UpdateHealthUI(int currentHealth)
     {
-        for (int i = 0; i < hearts.Length; i++)
-        {
-            if (i < currentHealth)
-            {
+        for (int i = 0; i < hearts.Length; i++) {
+            if (i < currentHealth) {
                 hearts[i].sprite = fullHeart;
-            }
-            else
-            {
+            } else {
                 hearts[i].sprite = emptyHeart;
             }
         }
@@ -79,13 +72,10 @@ public class Player : MonoBehaviour
 
     public void Heal(int healAmount)
     {
-        if (health + healAmount > 5)
-        {
+        if (health + healAmount > 5) {
             health = 5;
-        }
-        else
-        {
-            health = health + healAmount;
+        } else {
+            health = health + healAmount;   
         }
         UpdateHealthUI(health);
     }
--- Assets/Scripts/WaveSpawner.cs
+++ Assets/Scripts/WaveSpawner.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class WaveSpawner : MonoBehaviour
 {
     [System.Serializable]
-    public class Wave
+    public class Wave 
     {
         public GameObject[] enemies;
         public int count;
@@ -32,7 +32,7 @@ public class WaveSpawner : MonoBehaviour
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
-        sceneTransitions = FindAnyObjectByType<SceneTransitions>();
+        sceneTransitions = FindObjectOfType<SceneTransitions>();
         StartCoroutine(StartNextWave(currentWaveIndex));
     }
 
@@ -45,22 +45,17 @@ public class WaveSpawner : MonoBehaviour
     IEnumerator SpawnWave(int index)
     {
         currentWave = waves[index];
-        for (int i = 0; i < currentWave.count; i++)
-        {
-            if (player == null)
-            {
+        for (int i = 0; i < currentWave.count; i++) {
+            if (player == null) {
                 yield break;
             }
             GameObject randomEnemy = currentWave.enemies[Random.Range(0, currentWave.enemies.Length)];
             Transform randomSpot = spawnPoints[Random.Range(0, spawnPoints.Length)];
             Instantiate(randomEnemy, randomSpot.position, randomSpot.rotation);
 
-            if (i == currentWave.count - 1)
-            {
+            if (i == currentWave.count - 1) {
                 finishedSpawning = true;
-            }
-            else
-            {
+            } else {
                 finishedSpawning = false;
             }
 
@@ -70,18 +65,14 @@ public class WaveSpawner : MonoBehaviour
 
     private void Update()
     {
-        if (finishedSpawning == true && GameObject.FindGameObjectsWithTag("Enemy").Length == 0)
-        {
+        if (finishedSpawning == true && GameObject.FindGameObjectsWithTag("Enemy").Length == 0) {
             finishedSpawning = false;
 
             // if there is another wave
-            if (currentWaveIndex + 1 < waves.Length)
-            {
+            if (currentWaveIndex + 1 < waves.Length) {
                 currentWaveIndex++;
                 StartCoroutine(StartNextWave(currentWaveIndex));
-            }
-            else
-            {
+            } else {
                 // once waves are over, send in final boss
                 bossWave = true;
                 Instantiate(boss, bossSpawnPoint.position, bossSpawnPoint.rotation);
@@ -89,10 +80,8 @@ public class WaveSpawner : MonoBehaviour
             }
         }
 
-        if (bossWave == true)
-        {
-            if (GameObject.FindGameObjectsWithTag("Boss").Length == 0 && GameObject.FindGameObjectsWithTag("Enemy").Length == 0)
-            {
+        if (bossWave == true) {
+            if (GameObject.FindGameObjectsWithTag("Boss").Length == 0 && GameObject.FindGameObjectsWithTag("Enemy").Length == 0) {
                 sceneTransitions.LoadScene("Win");
             }
         }
